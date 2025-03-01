9f54845fccee58a28ea0ef5fa199f4bbf9074a60
tush-gt <tushita5072@gmail.com>
2025-03-01 13:18:42 +0530

    Home.js cards added
    Cards

--- czero/src/components/Home.js
+++ czero/src/components/Home.js
@@ -3,14 +3,21 @@ import "./Home.css"
 function Home()
 {
     return <>
-    <div class="body">
+    <div className="body">
        <header>
         <div class="title-box">
             <h1>CarbonZero</h1>
             <h4>Quantify. Reduce. Sustain</h4>
         </div>
         </header>
-        
+        <div className="cards">
+            <div>
+                <h2>What is Carbon Footprint?</h2>
+                <p className="card-content">
+                A carbon footprint is the total amount of greenhouse gases (GHGs) released into the atmosphere due to a person's or organization's actions. It includes emissions from burning fossil fuels for transportation, electricity, and other activities.
+                </p>
+            </div>
+        </div>
         <footer>
             <p>&copy; 2025 CarbonZero. All rights reserved.</p>
         </footer> 
