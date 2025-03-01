a336ddc7e3559721a19043e159a59efee72a5266
tush-gt <tushita5072@gmail.com>
2025-03-01 12:59:29 +0530

    Home and Nvabar major
    Navbar integrated wiht Home page
    Designed like In figma

--- czero/src/App.js
+++ czero/src/App.js
@@ -2,10 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <><Home /><Calculator /></>
+    <>
+    {/* <Calculator/> */}
+    <Navbar />
+    <Home/>
+    </>
   )
 }
 
