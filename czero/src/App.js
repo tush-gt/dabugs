d8bf6248d1f1c04299b8515ff8b4c0acf65041fa
Ankita53744 <223ankita0075@dbit.in>
2025-03-01 16:30:38 +0530

    commit
    hi

--- czero/src/App.js
+++ czero/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import React from "react";
+import ReactDOM from "react-dom";
+import Calculator from "./components/Calculator";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Signup from './components/Signup/Signup'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <><Home /></>
+    <>
+    <Navbar/>
+    <Home/>
+    <Calculator/> </>
   )
 }
 
--- czero/src/components/Home.js
+++ czero/src/components/Home.js
@@ -10,14 +10,7 @@ function Home()
             <h4>Quantify. Reduce. Sustain</h4>
         </div>
         </header>
-        <div className="cards">
-            <div>
-                <h2>What is Carbon Footprint?</h2>
-                <p className="card-content">
-                A carbon footprint is the total amount of greenhouse gases (GHGs) released into the atmosphere due to a person's or organization's actions. It includes emissions from burning fossil fuels for transportation, electricity, and other activities.
-                </p>
-            </div>
-        </div>
+        <div id="photo"></div>
         <footer>
             <p>&copy; 2025 CarbonZero. All rights reserved.</p>
         </footer> 
